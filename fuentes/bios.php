22879bd34b750d6bbe4591f689fdbe6f5efd1d7b
Dreyf <dreyfcesar@gmail.com>
2022-07-10 15:35:43 -0300

    Agrega el gestor de errores
    Cada vez que se produzca un error se van a guardar en un archivo los
    datos del error.

--- fuentes/bios.php
+++ fuentes/bios.php
@@ -7,4 +7,9 @@
     require $mow_carpeta_principal . '/Gestor/Autoload/Autoload.php';
     new Mow\Gestor\Autoload\Autoload();
 
+    // Instancia el gestor de errores
+    $gGuardarErrores = new Mow\Datos\Sistema\Errores\Guardar\GuardarTexto(new Mow\Datos\Archivos\Archivo($gestor_errores_archivo));
+    $gImprimirErrores = new Mow\Datos\Sistema\Errores\Imprimir\ImprimirTexto();
+    $gError = new Mow\Gestor\Sistema\Errores\Errores($gGuardarErrores, $gImprimirErrores);
+
 ?>
--- fuentes/config.php
+++ fuentes/config.php
@@ -1,6 +1,14 @@
 <?php
 
+    // Proyecto
+    $proyecto_carpeta_fuentes = __DIR__;
+    $proyecto_carpeta_principal = dirname($proyecto_carpeta_fuentes);
+    $proyecto_carpeta_registros = $proyecto_carpeta_principal . '/registros';
+
     // Mow
     $mow_carpeta_principal = '../../mow';
+
+    // Gestor Errores
+    $gestor_errores_archivo = $proyecto_carpeta_registros . '/errores.log';
 
 ?>
