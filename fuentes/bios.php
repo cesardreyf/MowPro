e4c433b3b96c9f9e8a97c7e8dc2a1b4c3a87417b
Dreyf <dreyfcesar@gmail.com>
2022-07-10 23:04:57 -0300

    Cambia los nombres de las variables

--- fuentes/bios.php
+++ fuentes/bios.php
@@ -8,16 +8,16 @@
     new Mow\Gestor\Autoload\Autoload();
 
     // Instancia el gestor de errores
-    $gConfigErrores = new Mow\Datos\Memoria\MemoriaSoloLectura('gestor_errores_config', $gestor_errores_config_array);
-    $gGuardarErrores = new Mow\Datos\Sistema\Errores\Guardar\GuardarTexto(new Mow\Datos\Archivos\Archivo($gestor_errores_archivo));
-    $gImprimirErrores = new Mow\Datos\Sistema\Errores\Imprimir\ImprimirTexto();
-    $gErrores = new Mow\Gestor\Sistema\Errores\Errores($gConfigErrores, $gGuardarErrores, $gImprimirErrores);
+    $gErroresConfig = new Mow\Datos\Memoria\MemoriaSoloLectura('gestor_errores_config', $gestor_errores_config_array);
+    $gErroresGuardar = new Mow\Datos\Sistema\Errores\Guardar\GuardarTexto(new Mow\Datos\Archivos\Archivo($gestor_errores_archivo));
+    $gErroresImprimir = new Mow\Datos\Sistema\Errores\Imprimir\ImprimirTexto();
+    $gErrores = new Mow\Gestor\Sistema\Errores\Errores($gErroresConfig, $gErroresGuardar, $gErroresImprimir);
 
     // Instancia el gestor de excepciones
-    $gConfigExcepciones = new Mow\Datos\Memoria\MemoriaSoloLectura('gestor_excepciones_config', $gestor_excepciones_config_array);
-    $gGuardarExcepciones = new Mow\Datos\Sistema\Excepciones\Guardar\GuardarTexto(new Mow\Datos\Archivos\Archivo($gestor_excepciones_archivo));
-    $gImprimirExcepciones = new Mow\Datos\Sistema\Excepciones\Imprimir\ImprimirTexto();
-    $gExcepciones = new Mow\Gestor\Sistema\Excepciones\Excepciones($gConfigExcepciones, $gGuardarExcepciones, $gImprimirExcepciones);
+    $gExcepcionesConfig = new Mow\Datos\Memoria\MemoriaSoloLectura('gestor_excepciones_config', $gestor_excepciones_config_array);
+    $gExcepcionesGuardar = new Mow\Datos\Sistema\Excepciones\Guardar\GuardarTexto(new Mow\Datos\Archivos\Archivo($gestor_excepciones_archivo));
+    $gExcepcionesImprimir = new Mow\Datos\Sistema\Excepciones\Imprimir\ImprimirTexto();
+    $gExcepciones = new Mow\Gestor\Sistema\Excepciones\Excepciones($gExcepcionesConfig, $gExcepcionesGuardar, $gExcepcionesImprimir);
 
     // Instancia el invocador
     $gInvocador = new Mow\Gestor\Invocador\Invocador(new Mow\Datos\Archivos\Carpeta($app_librerias_carpeta));
